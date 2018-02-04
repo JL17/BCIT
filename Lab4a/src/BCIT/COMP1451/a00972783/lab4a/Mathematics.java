4cd1e198e48e2a94f84289d92ae7b8722b18ca4c
Partslug <Partslug@Capt-PC>
2018-02-03 16:12:52 -0800

    Lab4a Complete

--- Lab4a/src/BCIT/COMP1451/a00972783/lab4a/Mathematics.java
+++ Lab4a/src/BCIT/COMP1451/a00972783/lab4a/Mathematics.java
@@ -1,9 +1,12 @@
 package BCIT.COMP1451.a00972783.lab4a;
 
+import java.util.Random;
+
 public class Mathematics {
 
 	public static final double PI = Math.PI;
 	public static final double ONE_FOOT_TO_KILOMETER_RATIO = 1 / 3280.8;
+	public static final int ZERO = 0;
 		
 	public Mathematics() {}	
 	
@@ -23,15 +26,58 @@ public class Mathematics {
 	}
 	
 	public double times(double first, double second) {
-		double product = first + second;
+		double product = first * second;
 		return product;
 	}
 	
+	public double subtract(double first, double second) {
+		double difference = first - second;
+		return difference;
+	}
 	
+	public int divide(int first, int second) {
+		
+		if(second == ZERO) {
+			return ZERO;
+		}
+		else {
+			int division = first / second;
+			return division;
+		}
+	}
 	
+	public int absoluteValue(int input) {
+		int abs = Math.abs(input);
+		return abs;
+	}
 	
+	public int getRandomNumberBetweenTenAndTwentyButNotFifteen(){
+		Random r = new Random();
+		boolean isFifteen = false;
+		int randomNumber = 0;
+		
+		while(!isFifteen)
+		{
+		randomNumber = r.nextInt(11) + 10;
+			if(randomNumber != 15)
+			{	
+				isFifteen = true;					
+			}
+		}
+		
+		return randomNumber;
+	}
 	
+	public double convertFeetToKilometers(double input) {
+		double conversion = input * ONE_FOOT_TO_KILOMETER_RATIO;
+		System.out.println(conversion);
+		return conversion;
+	}
 	
-	
+	public Integer doubleTheNumber(Integer input) {
+		Integer number = new Integer(input);
+		number *= 2;
+		return number;
+	}
 
 }
