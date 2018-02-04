6b199f13cec87096e24e346249e787063182650f
Partslug <Partslug@Capt-PC>
2018-02-03 17:06:10 -0800

    lab4a complete complete

--- Lab4a/src/BCIT/COMP1451/a00972783/lab4a/MathTest.java
+++ Lab4a/src/BCIT/COMP1451/a00972783/lab4a/MathTest.java
@@ -3,7 +3,6 @@ package BCIT.COMP1451.a00972783.lab4a;
 public class MathTest {
 
 	public static void main(String[] args) {
-		Mathematics math = new Mathematics();
 		
 		
 	}
--- Lab4a/src/BCIT/COMP1451/a00972783/lab4a/MathematicsTest.java
+++ Lab4a/src/BCIT/COMP1451/a00972783/lab4a/MathematicsTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.util.HashSet;
  
+@SuppressWarnings("unused")
 public class MathematicsTest {
 
 	private Mathematics math;
